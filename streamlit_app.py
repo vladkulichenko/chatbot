58864bb6abe67c513ba4efb9481295c610598272
vladkulichenko <87648495+vladkulichenko@users.noreply.github.com>
2024-08-23 12:57:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,82 @@
-import streamlit as st
-from openai import OpenAI
+from typing import Dict, Any
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+import streamlit as st
+from langchain import hub
+from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain.agents import AgentExecutor, create_react_agent, create_tool_calling_agent
+from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain_core.callbacks import StdOutCallbackHandler, BaseCallbackHandler
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory, StreamlitChatMessageHistory
+from langchain_ollama import ChatOllama
+from langchain.load.dump import dumps
+
+from langchain_openai import ChatOpenAI
+from prompts import appraisal_bot_prompt
+from tools import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOllama(temperature=0.5, model='llama3-groq-tool-use', model_kwargs={"seed": 42}, stream=True)
+# llm = ChatOpenAI(temperature=0, model='gpt-4o-mini')
+tools = [mls_tool]
+
+history = StreamlitChatMessageHistory()
+model = llm.bind_tools(tools)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            appraisal_bot_prompt
+        ),
+        ("placeholder", "{chat_history}"),
+        ("placeholder", "{tools}"),
+        ("placeholder", "{tool_names}"),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+agent = create_tool_calling_agent(model, tools, prompt)
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    return_intermediate_steps=True,
+    max_iterations=5,
+)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+agent_with_chat_history = RunnableWithMessageHistory(
+    agent_executor,
+    lambda session_id: history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+class MyCustomHandler(BaseCallbackHandler):
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    def on_tool_end(self, output: str, **kwargs: Any) -> Any:
+        """Run when tool ends running."""
+        print(output)
+        history.add_ai_message(f'tool_output: {output}')
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+avatars = {"human": "user", "ai": "assistant"}
+for msg in history.messages:
+    if not msg.content.__contains__("tool_output"):
+        st.chat_message(avatars[msg.type]).write(msg.content)
+
+if user_query := st.chat_input():
+    st.chat_message("user").write(user_query)
+    with st.chat_message("assistant"):
+        callback = MyCustomHandler()
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = (agent_with_chat_history.invoke({
+            "input": user_query,
+            "chat_history": history,
+        }, {'configurable': {'session_id': 'placeholder_id'}, "callbacks": [st_callback, callback]}))
+        st.write(response['output'])
